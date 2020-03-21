8eec65de9d49fb81cbe3c90e29b3071d34a87a25
KamilBuczek96 <buckam96@gmail.com>
2020-03-21 14:05:56 +0100

    Clearing repo

--- password_check.py
+++
@@ -1,6 +0,0 @@
-login = input('Login: ')
-password = input('Password: ')
-
-password_size = len(password)
-secret_password= password_size * '*'
-print(f'Hello {login}, your password {secret_password} is {password_size} letters long')
